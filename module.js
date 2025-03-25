948e7b6d428ab7846d0c81e820ed2b80c26c2bea
xPeppesrx <peppinowhite@outlook.it>
2025-03-25 13:33:53 +0100

    Update module.js

--- module.js
+++ module.js
@@ -13,7 +13,7 @@ class MyCustomCharacterSheet extends ActorSheet {
     static get defaultOptions() {
         return foundry.utils.mergeObject(super.defaultOptions(), {
             classes: ["dnd5e", "actor", "character", "my-custom-sheet"],
-            template: "modules/my-custom-sheet/templates/actor-sheet.html",
+            template: null, // Elimina il riferimento al file HTML
             width: 800,
             height: 700,
             tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "attributes" }]
@@ -25,14 +25,53 @@ class MyCustomCharacterSheet extends ActorSheet {
         const data = super.getData();
         console.log("My Custom Sheet | getData() chiamato!", data);
 
-        // Puoi aggiungere dati personalizzati qui
+        // Calcola i modificatori di caratteristica
+        data.strModifier = Math.floor((data.actor.data.data.abilities.str.value - 10) / 2);
+        data.dexModifier = Math.floor((data.actor.data.data.abilities.dex.value - 10) / 2);
+        data.conModifier = Math.floor((data.actor.data.data.abilities.con.value - 10) / 2);
+        data.intModifier = Math.floor((data.actor.data.data.abilities.int.value - 10) / 2);
+        data.wisModifier = Math.floor((data.actor.data.data.abilities.wis.value - 10) / 2);
+        data.chaModifier = Math.floor((data.actor.data.data.abilities.cha.value - 10) / 2);
+
         return data;
-    }
+      }
 
-    /** @override */
-    activateListeners(html) {
-        super.activateListeners(html);
+      /** @override */
+      activateListeners(html) {
+          super.activateListeners(html);
 
-        // Gestisci gli eventi personalizzati qui
+          // Elimina tutto il contenuto esistente
+          html.empty();
+
+          // Crea un elemento h1 per il nome del personaggio
+          const nameElement = $("<h1>" + this.actor.name + "</h1>");
+
+          // Crea un div per gli attributi
+          const attributesDiv = $("<div class='attributes'></div>");
+
+          // Funzione per creare un elemento attributo
+          const createAttributeElement = (ability, value, modifier) => {
+              return $(`
+                  <div class='attribute'>
+                      <label>${ability}</label>
+                      <span>${value}</span>
+                      <span>(${modifier})</span>
+                  </div>
+              `);
+          };
+
+          // Crea elementi per ogni attributo
+          const strElement = createAttributeElement("Forza", this.actor.data.data.abilities.str.value, this.getData().strModifier);
+          const dexElement = createAttributeElement("Destrezza", this.actor.data.data.abilities.dex.value, this.getData().dexModifier);
+          const conElement = createAttributeElement("Costituzione", this.actor.data.data.abilities.con.value, this.getData().conModifier);
+          const intElement = createAttributeElement("Intelligenza", this.actor.data.data.abilities.int.value, this.getData().intModifier);
+          const wisElement = createAttributeElement("Saggezza", this.actor.data.data.abilities.wis.value, this.getData().wisModifier);
+          const chaElement = createAttributeElement("Carisma", this.actor.data.data.abilities.cha.value, this.getData().chaModifier);
+
+          // Aggiungi gli elementi degli attributi al div degli attributi
+          attributesDiv.append(strElement, dexElement, conElement, intElement, wisElement, chaElement);
+
+          // Aggiungi gli elementi alla scheda
+          html.append(nameElement, attributesDiv);
+      }
     }
-}
